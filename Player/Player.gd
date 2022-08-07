7df382d135f9e427a74bb98ab920cfa94680fac9
Ben McHone <ben@mchone.dev>
2022-08-07 17:13:09 -0500

    Basic AI

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,29 @@
-extends Node2D
+extends KinematicBody2D
+
+const ai_update_freq = 0.25;
 
 var movement_speed = 500
+var ai_pressed = false
+var ai_last_update = 0
 export(String) var action
+
+var ball: KinematicBody2D
+
+func _ready():
+	ball = get_parent().find_node("Ball")
 
 func _physics_process(delta):
 	var movement = movement_speed
-	if Input.is_action_pressed(action):
+	if !action:
+		ai_last_update += delta
+		if ai_last_update > ai_update_freq:
+			ai_pressed = should_rise_ai()
+			ai_last_update = 0
+		if ai_pressed:
+			movement = -movement
+	elif Input.is_action_pressed(action):
 		movement = -movement
-	$PhysicsBody.move_and_collide(Vector2(0, movement) * delta)
+	move_and_slide(Vector2(0, movement))
+
+func should_rise_ai():
+	return position.y - ball.position.y > 50
